254c58e3afd83ba3ffdb34bf8cc06ccdec0aa9c8
0xUgochukwu <0xUgochukwu@gmail.com>
2023-08-02 10:19:40 -0700

    pre-order

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,8 +11,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		func(tree->value);
-		binary_tree_preorder(tree->left);
-		binary_tree_preorder(tree->right);
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
