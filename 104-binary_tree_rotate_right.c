7370732ffd60bb730ed829fb78c534d00e544c82
0xUgochukwu <0xUgochukwu@gmail.com>
2023-08-04 05:23:31 -0700

    rotr

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -2,25 +2,25 @@
 
 /**
  * binary_tree_rotate_right - performs a right-rotation on a binary tree
- * @tree: a pointer to the root node of the tree to check
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: pointer to the new root node of the tree once rotated
  *
- * Return: Pointer to the new root node of the tree once rotated
- *         NULL upon failure
  */
+
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *l_child;
+	binary_tree_t *tmp;
 
 	if (!tree || !tree->left)
 		return (NULL);
 
-	l_child = tree->left;
-	l_child->parent = tree->parent;
-	tree->parent = l_child;
-	tree->left = l_child->right;
-	if (l_child->right)
-		l_child->right->parent = tree;
-	l_child->right = tree;
+	tmp = tree->left;
+	tmp->parent = NULL;
+	tree->parent = tmp;
+	tree->left = tmp->right;
+	if (tree->left)
+		tree->left->parent = tree;
+	tmp->right = tree;
 
-	return (l_child);
+	return (tmp);
 }
